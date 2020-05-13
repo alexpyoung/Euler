8019ed7f72cfa8acbf310a9856cd8494b89bed5a
Alex Young <alexyoung1992@gmail.com>
2020-05-13 11:22:25 -0400

    refactor(xcode): update for Swift 5, Xcode 11

--- Euler/Euler.h
+++ Euler/Euler.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Alex Young. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Euler.
 FOUNDATION_EXPORT double EulerVersionNumber;
